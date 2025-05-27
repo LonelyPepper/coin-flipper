7cd9d3cde1e66b630fc262263e3091057183ef2d
ignacy.niew <ignacy.nieweglowski@gmail.com>
2025-05-27 13:48:21 -0400

    added base_32 helper functions

---
+++ app/helper/base_32.js
@@ -0,0 +1,25 @@
+function base32Encode(reported, actual) {
+  const str = reported.toString() + " " + actual.toString();
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
+  const bytes = new TextEncoder().encode(str);
+
+  let bits = "";
+  let output = "";
+
+  for (let byte of bytes) {
+    bits += byte.toString(2).padStart(8, "0");
+  }
+
+  for (let i = 0; i < bits.length; i += 5) {
+    const chunk = bits.substring(i, i + 5).padEnd(5, "0");
+    output += alphabet[parseInt(chunk, 2)];
+  }
+
+  while (output.length % 8 !== 0) {
+    output += "=";
+  }
+
+  return output;
+}
+
+console.log(base32Encode(15,16));
